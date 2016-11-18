88c14f6a87040712fffb6890f63762ac1590dcb2
Guillaume Claret <dev@clarus.me>
2016-11-18 14:07:14 +0100

    Only synchronous actions for the devtool

--- src/devtools/controller.js
+++ src/devtools/controller.js
@@ -2,32 +2,10 @@
 import * as Ship from 'redux-ship';
 import * as Model from './model';
 
-export type Action = {
-  type: 'AddLog',
-  action: mixed,
-  snapshot: Ship.Snapshot<mixed, mixed>,
-} | {
-  type: 'SelectLog',
-  logIndex: number,
-} | {
-  type: 'SelectSnapshotItem',
-  snapshotItem: Ship.SnapshotItem<mixed, mixed>,
-};
+export type Action = Model.Commit;
 
 type Control<A> = Ship.Ship<*, Model.Commit, Model.State, A>;
 
 export function* control(action: Action): Control<void> {
-  switch (action.type) {
-    case 'AddLog':
-      yield* Ship.commit(action);
-      return;
-    case 'SelectLog':
-      yield* Ship.commit(action);
-      return;
-    case 'SelectSnapshotItem':
-      yield* Ship.commit(action);
-      return;
-    default:
-      return;
-  }
+  yield* Ship.commit(action);
 }
