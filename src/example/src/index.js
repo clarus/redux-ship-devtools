ae2d26125a265c196020a716ad2b0af48c096b2f
Guillaume Claret <dev@clarus.me>
2016-11-28 07:22:39 +0100

    0.1.0

--- src/example/src/index.js
+++ src/example/src/index.js
@@ -3,26 +3,11 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Ship from 'redux-ship';
+import {inspectControl} from 'redux-ship-devtools';
 import Index from './view';
 import store from './store';
 import * as Controller from './controller';
 import * as Effect from './effect';
-
-function inspectControl<Action, Effect, Commit, State>(
-  control: (action: Action) => Ship.Ship<Effect, Commit, State, void>
-): (action: Action) => Ship.Ship<Effect, Commit, State, void> {
-  return function* (action) {
-    const {snapshot} = yield* Ship.snap(control(action));
-    window.postMessage({
-      type: 'ReduxShipDevtools',
-      payload: {
-        action,
-        snapshot,
-      },
-      version: 1,
-    }, '*');
-  };
-}
 
 function dispatch(action: Controller.Action): void {
   Ship.run(Effect.run, store, inspectControl(Controller.control)(action));
