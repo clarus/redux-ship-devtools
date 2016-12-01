08f10299a406a8d1c20ab6df5c5bbb8c315328ba
Guillaume Claret <dev@clarus.me>
2016-12-01 13:35:33 +0100

    Add console loggers

--- src/example/src/index.js
+++ src/example/src/index.js
@@ -4,13 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Ship from 'redux-ship';
 import {inspectControl} from 'redux-ship-devtools';
+import {logControl} from 'redux-ship-logger';
 import Index from './view';
 import store from './store';
 import * as Controller from './controller';
 import * as Effect from './effect';
 
 function dispatch(action: Controller.Action): void {
-  Ship.run(Effect.run, store, inspectControl(Controller.control)(action));
+  Ship.run(Effect.run, store, inspectControl(logControl(Controller.control))(action));
 }
 
 function render() {
